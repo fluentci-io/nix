c487f56d085bffca12363587d9bdc30f477aff46
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-09-30 10:26:13 +0300

    make it compatible with Dagger Zenith

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-import Client from "https://sdk.fluentci.io/v0.1.9/mod.ts";
+import Client from "https://sdk.fluentci.io/z1/mod.ts";
 export default Client;
-export { connect, Container } from "https://sdk.fluentci.io/v0.1.9/mod.ts";
+export { connect, Container } from "https://sdk.fluentci.io/z1/mod.ts";
